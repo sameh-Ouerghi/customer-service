8f36405d3225549d162d8ed8b07b2cd4202ac87f
Ouerghi <samehouerghi6@gmail.com>
2021-10-29 18:31:17 -0400

    beanche dev1

--- src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package org.sid.customerservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.sid.customerservice.entities.Customer;
+        import org.sid.customerservice.repository.CustomerRepository;
+        import org.springframework.boot.CommandLineRunner;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,15 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(new Customer(null, "Mohamed", "med@gmail.com"));
+            customerRepository.save(new Customer(null, "Hassan", "Hassen@gmail.com"));
+            customerRepository.save(new Customer(null, "Salima", "salima@gmail.com"));
+            customerRepository.findAll().forEach(c -> {
+                System.out.println(c.toString());
+            });
+        };
+    }
 }
